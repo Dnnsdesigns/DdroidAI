64a501805c71d1692f56cd101daa053c5176d312
Dennis Duncan <144344247+Dnnsdesigns@users.noreply.github.com>
2024-08-13 17:25:35 -0700

    Update agent.js

--- backend/routes/agent.js
+++ backend/routes/agent.js
@@ -1,1 +1,7 @@
+const express = require('express');
+const router = express.Router();
+const agentController = require('../agentController');
 
+router.post('/', agentController.createAgent);
+
+module.exports = router;
